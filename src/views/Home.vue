ba86c27b32455f40b0c84b91d168c502d63df33f
jerry <email123333@163.com>
2020-08-18 08:58:33 +0800

    配置基本css和路由

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,94 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="top">Origin</div>
+    <div class="banner">
+      <img src="~assets/img/home/top.jpg" alt="">
+    </div>
+    <div class="mainbody">
+      <div class="plane">
+        <img src="~assets/img/home/plane.png" alt="">
+      </div>
+      <div class="tabbar">
+        <div class="line">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+    <div class="footer"></div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
+
+<style>
+  .home{
+    width: 100%;
+    background: url("~assets/img/home/backgroundimg.jpg");
+    height: 100%;
+  }
+  .home div{
+    width: 90%;
+    margin: 0 auto;
+    margin-bottom: 10px;
+  }
+  .top{
+    height: 90px;
+    line-height: 90px;
+    color: var(--color-text);
+    font-size: 50px;
+  }
+  .banner img{
+    width: 100%;
+    height: 550px;
+  }
+  .mainbody{
+    position: relative;
+    height: 200px;
+  }
+  .plane{
+    position: absolute;
+    top: 20px;
+    left: 0;
+  }
+  .mainbody .tabbar{
+    margin: 0 auto;
+    width: 80%;
+    height: 200px;
+    /*background-color: #ff5777;*/
+  }
+  .tabbar .line{
+    width: 20px;
+    background-color: #cccccc;
+    height: 200px;
+  }
+</style>
